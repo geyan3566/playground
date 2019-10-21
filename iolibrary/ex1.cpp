b993da73b4514f3a97ef93bcd409e9f9c47d7935
geyan <geyan3566@gmail.com>
2019-10-20 23:48:49 -0400

    second commit

--- iolibrary/ex1.cpp
+++ iolibrary/ex1.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+int &test (int &input)
+{
+   input += 1;
+   int *a = &input; 
+   return *a; 
+}
 
 int main()
 {
-   int i;
+   int i,j;
    std::cout << "Please input your integer:";
    std::cin >> i;
-   std::cout << "The number is  " << i << '\n'; 
+   std::cout << "The number is  " << i << '\n';
+
+   // // test of end of file 
+   // while(std::cin >> j)
+   // {
+   //    std::cout << j << ' ';
+   // }
+   // std::cin.clear();
+   // std::cin >> c;
+   // std::cout << c;
+   j = test(i);
+   std::cout << j;
    return 0;
 }
+
